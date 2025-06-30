af7e31dedc8971224e0e86329a55ea5d03496cc3
zeshan2001 <zeshanbh@gmail.com>
2025-06-30 14:21:48 +0300

    startButton eventListener add

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Speedy Fingers!!')
+// Variables
+
+const startButton = document.querySelector('#start-btn')
+let difficulty
+
+// Methods
+
+// Events
+startButton.addEventListener('click', () => {
+  const timeDropdown = document.querySelector('#time').value
+  const difficultyDropdown = document.querySelector('#difficulty').value
+
+  console.log(timeDropdown)
+  console.log(difficultyDropdown)
+})
