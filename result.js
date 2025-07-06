523022275f5f103e4d44d08b92244ef0662d05e3
zeshan2001 <zeshanbh@gmail.com>
2025-07-06 19:00:23 +0300

    result.js modified

--- result.js
+++ result.js
@@ -3,9 +3,7 @@ import Play from './play.js'
 const extractPara = new URLSearchParams(location.search)
 const time = extractPara.get('time')
 const difficulty = extractPara.get('difficulty')
-let randomCharList = []
-let randomWordList = []
-
+let currentPassage = 'Typing tests are a fun way to improve your speed.'
 const playObj = new Play(difficulty, parseInt(time))
 
 // ********************* Cached elements references ********************* //
@@ -13,16 +11,9 @@ const timeElement = document.querySelector('#time')
 const levelElement = document.querySelector('#level')
 const speedWPMElement = document.querySelector('#speed-wpm')
 const accuracyElement = document.querySelector('#accuracy')
-let randomTextElement = (document.querySelector('#check-text').innerText =
-  playObj.randomSentence()) // random text
-const userTextElement = document.querySelector('#typing-test')
-const highlightOutput = document.querySelector('#highlight-output')
 
-// randomTextElement = playObj.randomSentence()
-timeElement.innerText = `${playObj.getTime()}:00`
-levelElement.innerText = playObj.getLevel() // level
-randomCharList = randomTextElement.split('')
-randomWordList = randomTextElement.split(' ')
+const passageEl = document.querySelector('#passage')
+const inputEl = document.querySelector('#input')
 
 // ********************* Methods ********************* //
 
@@ -50,72 +41,26 @@ const timeCountDown = (minutes) => {
   let interval = setInterval(updateTimer, 1000)
 }
 
-const highlightChar = (userCharList) => {
-  const highlighted = userCharList.map((char, index) => {
-    if (char === randomCharList[index]) {
-      return `<span style="color: green;">${randomCharList[index]}</span>`
+const render = () => {
+  const userInput = inputEl.value
+  let highlighted = ''
+  for (let i = 0; i < currentPassage.length; i++) {
+    if (userInput[i] == null) {
+      highlighted += `<span>${currentPassage[i]}</span>`
+    } else if (userInput[i] === currentPassage[i]) {
+      highlighted += `<span class="correct">${currentPassage[i]}</span>`
     } else {
-      return `<span style="color: red;">${randomCharList[index] || ' '}</span>`
+      highlighted += `<span class="incorrect">${currentPassage[i]}</span>`
     }
-  })
-  return highlighted
+  }
+  passageEl.innerHTML = highlighted
 }
-
-const countCorrectedWord = (wordList) => {
-  let count = 0
-  wordList.forEach((el, index) => {
-    if (el === randomWordList[index]) {
-      count++
-    }
-  })
-  return count
-}
-// const countCorrectedChars = (userCharList) => {
-//   let count = 0
-//   userCharList.forEach((el, index) => {
-//     if (el === randomCharList[index]) {
-//       count++
-//     }
-//   })
-//   return count
-// }
-let startTimer = 1
 
 // ************ userTyping ************* //
 
 const userTyping = () => {
-  startTimer--
-  if (startTimer === 0) {
-    timeCountDown(playObj.getTime()) // timer
-  }
-
-  const userCharList = userTextElement.textContent.split('')
-
-  const highlighted = highlightChar(userCharList)
-
-  const correctedChars = playObj.countCorrectedChars(
-    userCharList,
-    randomCharList
-  )
-  let speedWPM = playObj.calculateSpeedWPM(correctedChars, playObj.getTime()) // speed
-  let accuracy = playObj.calculateAccuracy(correctedChars, userCharList.length) // accuracy
-
-  //Add remaining (untyped) chars as default color
-  for (let i = userCharList.length; i < randomCharList.length; i++) {
-    highlighted.push(`<span style="color: gray;">${randomCharList[i]}</span>`)
-  }
-
-  speedWPMElement.innerText = speedWPM || 0
-  accuracyElement.innerText = accuracy || 0
-  highlightOutput.innerHTML = highlighted.join('')
+  render()
 }
 
 // ********************* Events ********************* //
-addEventListener('load', () => {
-  let highlighted = []
-  for (let i = 0; i < randomCharList.length; i++) {
-    highlighted.push(`<span style="color: gray;">${randomCharList[i]}</span>`)
-  }
-  highlightOutput.innerHTML = highlighted.join('')
-})
-userTextElement.addEventListener('input', userTyping)
+inputEl.addEventListener('input', userTyping)
